beb6aac7ab99ff920c54bc24947e2c8ed64078c9
sigvaldr <sigvaldrnotthrafn@proton.me>
2026-03-16 01:58:13 -0400

    cleanup

--- admin/constants/broadcast.ts
+++
@@ -1,7 +0,0 @@
-
-export const BROADCAST_CHANNELS = {
-    BENCHMARK_PROGRESS: 'benchmark-progress',
-    OLLAMA_MODEL_DOWNLOAD: 'ollama-model-download',
-    SERVICE_INSTALLATION: 'service-installation',
-    SERVICE_UPDATES: 'service-updates',
-}
--- admin/constants/kv_store.ts
+++
@@ -1,3 +0,0 @@
-import { KVStoreKey } from "../types/kv_store.js";
-
-export const SETTINGS_KEYS: KVStoreKey[] = ['chat.suggestionsEnabled', 'chat.lastModel', 'ui.hasVisitedEasySetup', 'system.earlyAccess', 'ai.assistantCustomName'];
